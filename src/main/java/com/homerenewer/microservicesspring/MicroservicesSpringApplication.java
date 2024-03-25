1f7193cc7e8fce4a0766fc73e1b7f36d139afec8
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2024-03-25 17:55:33 +0000

    firt commit

---
+++ src/main/java/com/homerenewer/microservicesspring/MicroservicesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.homerenewer.microservicesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homerenewer/microservicesspring/MicroservicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homerenewer.microservicesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
